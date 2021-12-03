e2df24c00954f0190119e29905f2116a1b4ee98a
Kayman Brusse <kaymanbrusse@gmail.com>
2021-12-02 21:28:58 -0500

    Setup and code for Day 2

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,3 +1,34 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn calculate_position(filename: &str) -> (i32, i32) {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    let mut depth = 0;
+    let mut position = 0;
+    let mut aim = 0;
+    for line in reader.lines() {
+        let line  = line.unwrap();
+        let mut line = line.split_whitespace();
+        let command = line.next().unwrap();
+        let dst: i32 = line.next().unwrap().parse().unwrap();
+        match command {
+            "forward" => {
+                position = position + dst;
+                depth = depth + aim*dst;
+            },
+            "up" => aim = aim - dst,
+            "down" => aim = aim + dst,
+            _ => println!("Oops!"),
+        }
+    }
+
+    return (depth, position)
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let (d, p) = calculate_position("input.txt");
+    println!("{}", d*p)
 }
